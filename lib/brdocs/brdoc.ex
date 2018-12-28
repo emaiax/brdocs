6be96e42cf8b9e5663c2c9c3f7b4e0bc3f79d2e4
Eduardo Maia <eduvimaia@gmail.com>
2018-12-27 22:16:24 -0200

    removing unecessary comment

--- lib/brdocs/brdoc.ex
+++ lib/brdocs/brdoc.ex
@@ -1,10 +1,8 @@
 defmodule BrDocs.BrDoc do
-  @docs [:cpf, :cnpj]
-
   @moduledoc """
   A struct containing value and kind of the doc.
 
-  * `kind` - An atom to identify which Brazilian doc kind is being held: #{Enum.map_join(@docs, ", ", &"`#{inspect &1}`")}.
+  * `kind` - An atom to identify which Brazilian doc kind is being held.
   * `value` - A string containing the raw or formatted value of the doc.
   """
 
