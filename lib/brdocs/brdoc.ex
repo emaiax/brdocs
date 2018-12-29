a92480519efd22d5a074b8f43064fdba53f2fa89
Eduardo Maia <eduvimaia@gmail.com>
2018-12-29 15:39:57 -0200

    [ebert] fixed documentation calls

--- lib/brdocs/brdoc.ex
+++ lib/brdocs/brdoc.ex
@@ -5,7 +5,7 @@ defmodule BrDocs.BrDoc do
   A struct containing value and kind of the doc.
 
   * `kind` - An atom to identify which Brazilian doc kind is being held. It must be one of #{
-    Enum.map_join(@available_docs, ", ", &"`#{inspect(&1)}`")
+    Enum.map_join(@available_docs, ", ", &("`#{inspect(&1)}`"))
   }.
   * `value` - A string containing the raw or formatted value of the doc.
   """
