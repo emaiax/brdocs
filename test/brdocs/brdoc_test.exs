4bd4a3a32abfabf7822b3713aa9ca82ce21967b7
Eduardo Maia <eduvimaia@gmail.com>
2018-12-29 17:26:58 -0200

    [format] format spec

--- test/brdocs/brdoc_test.exs
+++ test/brdocs/brdoc_test.exs
@@ -2,6 +2,6 @@ defmodule BrDocs.BrDocTest do
   use ExUnit.Case, async: true
 
   test "available docs" do
-    assert "`:cpf`, `:cnpj`" == BrDocs.BrDoc.formatted_available_docs
+    assert "`:cpf`, `:cnpj`" == BrDocs.BrDoc.formatted_available_docs()
   end
 end
